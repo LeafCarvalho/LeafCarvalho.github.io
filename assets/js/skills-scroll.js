cab5e7e1ea68e1d81cb56fac3b87a5c1b12c4862
Rafael Silva <rafael.silva@maiscode.com.br>
2023-10-19 16:22:57 -0300

    Revisando Insights e excluindo id da url

--- assets/js/skills-scroll.js
+++ assets/js/skills-scroll.js
@@ -27,3 +27,18 @@ window.addEventListener('scroll', () => {
         hideProgress();
     }
 })
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
